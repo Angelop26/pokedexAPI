11e823d2a2b0bf64eef583f8ddecec6a31a4d1f7
Angelop26 <angeloprospero26@gmail.com>
2025-03-25 13:24:48 +0100

    add background

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-ciao 123
+<div class="container">
 
+</div>
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
