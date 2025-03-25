44cd43fdb7de184b327c164dd18b27fe7dd27dce
Angelop26 <angeloprospero26@gmail.com>
2025-03-25 13:55:44 +0100

    add api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-    var url = "";
+    var url = "https://pokeapi.co/api/v2/pokemon";
     $.ajax({
-
+        url: url,
+        type: "post",
+        dataType: "json",
+        success: function(result){
+            alert("success")
+        },
+        error:function(result){
+            console.log(result)
+        }
     })
 })
